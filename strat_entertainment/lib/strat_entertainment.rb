9c3b43a45efd1b96143be526677b1a0bdddfd845
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-12 22:00:42 +0000

    initial commit

---
+++ strat_entertainment/lib/strat_entertainment.rb
@@ -0,0 +1,6 @@
+require "strat_entertainment/version"
+
+module StratEntertainment
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ strat_entertainment/lib/strat_entertainment/version.rb
@@ -0,0 +1,3 @@
+module StratEntertainment
+  VERSION = "0.1.0"
+end
